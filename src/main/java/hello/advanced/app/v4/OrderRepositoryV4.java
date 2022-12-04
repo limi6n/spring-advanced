0585a35901b08737a8805a34b5e6f3373c466b87
limi6n <sh889522!>
2022-12-04 17:50:52 +0900

    Template Method Pattern

---
+++ src/main/java/hello/advanced/app/v4/OrderRepositoryV4.java
@@ -0,0 +1,38 @@
+package hello.advanced.app.v4;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV4 {
+
+    private final LogTrace trace;
+
+    public void save(String itemId) {
+
+        AbstractTemplate<Void> template = new AbstractTemplate<Void>(trace) {
+            @Override
+            protected Void call() {
+                // 저장 로직
+                if (itemId.equals("ex")) {
+                    throw new IllegalStateException("예외 발생!");
+                }
+                sleep(1000); // 상품을 저장하는데 1초 정도 걸린다고 가정
+                return null;
+            }
+        };
+        template.execute("OrderRepository.save()");
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
